1531172cf919bcbba204ff795b1c370f0aab992b
unknown <nguyentantaitcag2000@gmail.com>
2023-11-20 10:22:06 +0700

    f

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -253,7 +253,7 @@
                     <div class="modal-dialog modal-xl">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Cập nhất chủ trọ</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Cập nhật chủ trọ</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
